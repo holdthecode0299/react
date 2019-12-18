b40496765f8b83ca4f75439e2569116a8d33fe14
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2019-12-17 23:02:29 -0500

    console logging response from weather api

--- my-app/src/pages/Forecast.js
+++ my-app/src/pages/Forecast.js
@@ -1,35 +1,43 @@
 import React, { Component } from "react"; 
-import API from "../utils/API";
+// import API from "../utils/API";
 
 class Forecast extends React.Component  {
 
 
-   getWeather = (e) => {
-       e.preventDefault();
-    const getWeather = async () => {
-        const call = await fetch("https://api.openweathermap.org/data/2.5/weather?zip=20001,us&appid=5735b3e048f694014a7f656569a5b4d4");
-        // .then(res => res.json())
-        const data = await call.json();
-        console.log(data)
-        // .then(data => console.log("Data List Loaded", data.list))
-    } 
+//    getWeather = (e) => {
+//        e.preventDefault();
+//     const getWeather = async () => {
+//         const call = await fetch("https://api.openweathermap.org/data/2.5/weather?zip=20001,us&appid=5735b3e048f694014a7f656569a5b4d4");
+//         // .then(res => res.json())
+//         const data = await call.json();
+//         console.log(data)
+//         // .then(data => console.log("Data List Loaded", data.list))
+//     } 
+
+    componentWillMount(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?zip=20001,us&appid=5735b3e048f694014a7f656569a5b4d4")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+        })
+    }
     
-  
+    render() {
+        return (
+        <div>
+            <h1>Weather Application</h1>
+        </div>
+        )
+    
+    };
       
 }
 
-render() {
-    return (
-    <div>
-        <h1>Weather Application</h1>
-    </div>
-    )
 
-};
 
     
 
-}
+
 
 
 
